d8328bdc3ac14fff0bf253fda715bbbf7ba1d4dd
Jason <git@jasoncameron.dev>
2024-06-28 12:00:52 -0400

    Grant proper perms to db user

--- dmoj/scripts/mariadb/init.sh
+++ dmoj/scripts/mariadb/init.sh
@@ -1,5 +1,8 @@
 sudo mariadb
-CREATE DATABASE "$MYSQL_DATABASE" DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS "$MYSQL_DATABASE" DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_general_ci;
 GRANT ALL PRIVILEGES ON "$MYSQL_DATABASE".* TO "$MYSQL_USER"@'localhost' IDENTIFIED BY "$MYSQL_PASSWORD";
+# shellcheck disable=SC2035 # this is in a mariadb shell so it wont glob
+GRANT RELOAD, PROCESS ON *.* TO "$MYSQL_USER"@'%';
+FLUSH PRIVILEGES;
 exit
 mariadb-tzinfo-to-sql /usr/share/zoneinfo | sudo mariadb -u root mysql
