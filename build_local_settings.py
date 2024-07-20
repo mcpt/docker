ac152b5ea194b74a199f35528e0e232d91e4ccd8
Jason <git@jasoncameron.dev>
2024-07-20 01:03:53 -0400

    Forgot how the file is used LOL
    replaced with adding the raw handlers to CI pipeline

--- build_local_settings.py
+++
@@ -1,8 +0,0 @@
-try:
-	with open(os.path.join(os.path.dirname(__file__), 'local_settings.py')) as f:
-		exec(f.read(), globals())
-except IOError:
-	pass
-
-LOGGING['handlers']['bridge']['filename'] = '/dev/null'
-LOGGING['handlers']['all']['filename'] = '/dev/null'
