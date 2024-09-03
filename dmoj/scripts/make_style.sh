e504a80ed4057f672157ba66b4987d741c59a770
Jason <git@jasoncameron.dev>
2024-09-03 17:57:33 -0400

    add makestyle & change location to /scripts

---
+++ dmoj/scripts/make_style.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Script stolen from the DMOJ project, all credit goes to them. This is simply located so I can bring it into the container easily.
+if ! [ -x "$(command -v sass)" ]; then
+  echo 'Error: sass is not installed.' >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v postcss)" ]; then
+  echo 'Error: postcss is not installed.' >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v autoprefixer)" ]; then
+  echo 'Error: autoprefixer is not installed.' >&2
+  exit 1
+fi
+
+cd "$(dirname "$0")" || exit
+
+build_style() {
+  echo "Creating $1 style..."
+  cp resources/vars-$1.scss resources/vars.scss
+  sass resources:sass_processed
+  postcss sass_processed/style.css sass_processed/martor-description.css sass_processed/select2-dmoj.css --verbose --use autoprefixer -d $2
+}
+
+build_style 'default' 'resources'
+build_style 'dark' 'resources/dark'
