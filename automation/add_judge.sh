5c99be396c09a73665fc72f22cd9943c55091679
Jason Cameron <git@jasoncameron.dev>
2024-09-11 00:21:11 -0400

    Fix: Limit judge length to 100 chars (varchar 100)
    Signed-off-by: Jason Cameron <git@jasoncameron.dev>

--- automation/add_judge.sh
+++ automation/add_judge.sh
@@ -45,10 +45,10 @@ EOF
 
 rand=$(head -c 75 /dev/urandom | tr -d '\0')
 # Convert bytes to Base64
-JUDGE_AUTH_KEY=$(echo "$rand" | base64 | tr -d '\n')
+JUDGE_AUTH_KEY=$(echo "$rand" | base64 | tr -d '\n' | cut -c -100 ) # db type is varchar(100)
 
 notify "Attempting to create new judge $JUDGE_NAME"
-run_single_command_on_site "python3 manage.py shell -c 'from judge.models import Judge; Judge.objects.create(name=\"$JUDGE_NAME\", auth_key=\"$JUDGE_AUTH_KEY\")'"
+run_single_command_on_site "python3 manage.py shell -c '''from judge.models import Judge; Judge.objects.create(name=\"$JUDGE_NAME\", auth_key=\"$JUDGE_AUTH_KEY\")'''"
 
 notify "Judge $JUDGE_NAME's DB obj was created"
 
