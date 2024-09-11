b759e46ee7682b5a2ca3af74e8785f7c6678b8d2
Jason Cameron <git@jasoncameron.dev>
2024-09-11 00:00:46 -0400

    Keep ssh in foreground
    Signed-off-by: Jason Cameron <git@jasoncameron.dev>

--- automation/add_judge.sh
+++ automation/add_judge.sh
@@ -36,7 +36,7 @@ function run_single_command_on_site() {
   NODE_IP=$(docker node inspect --format '{{ .Status.Addr  }}' "$SITE_NODE")
 
   # run the command
-  sudo ssh -f -q -o BatchMode=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "root@$NODE_IP" <<EOF
+  sudo ssh -q -o BatchMode=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "root@$NODE_IP" <<EOF
   # get id of the site container
   SITE_ID=\$(docker ps --filter name=wlmoj_site --format "{{.ID}}")
   docker exec -it \$SITE_ID /bin/bash -c "$1"
