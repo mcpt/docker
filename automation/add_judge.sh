ff3f1c419d019fabeec6991b3123d35eafc52869
Jason Cameron <git@jasoncameron.dev>
2024-09-11 00:35:43 -0400

    Fix:  Ignore failure if the homeshare already exists
    Signed-off-by: Jason Cameron <git@jasoncameron.dev>

--- automation/add_judge.sh
+++ automation/add_judge.sh
@@ -48,6 +48,7 @@ rand=$(head -c 75 /dev/urandom | tr -d '\0')
 JUDGE_AUTH_KEY=$(echo "$rand" | base64 | tr -d '\n' | cut -c -90 ) # db type is varchar(100)
 
 notify "Attempting to create new judge $JUDGE_NAME"
+# Warning for future devs: THE LINE BELOW IS A PAIN TO WRITE/DEAL WITH. I KNOW IT'S UGLY. I'M SORRY.
 run_single_command_on_site "python3 manage.py shell -c 'from judge.models import Judge; Judge.objects.create(name=\"'\"$JUDGE_NAME\"'\", auth_key=r\"'\"$JUDGE_AUTH_KEY\"'\")'"
 
 notify "Judge $JUDGE_NAME's DB obj was created"
