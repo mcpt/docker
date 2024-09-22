72797e3f3ffaa303a0f7602e57da4b2a982fc242
Jason Cameron <git@jasoncameron.dev>
2024-09-22 12:54:27 -0400

    Fix: Run judge bridge on 0.0.0.0
    Signed-off-by: Jason Cameron <git@jasoncameron.dev>

--- local_settings.py
+++ local_settings.py
@@ -131,7 +131,7 @@ TERMS_OF_SERVICE_URL = '//mcpt.ca/tos'  # Use a flatpage.
 # The judge connection address and port; where the judges will connect to the site.
 # You should change this to something your judges can actually connect to
 # (e.g., a port that is unused and unblocked by a firewall).
-BRIDGED_JUDGE_ADDRESS = [('bridged', 9999)]
+BRIDGED_JUDGE_ADDRESS = [('0.0.0.0', 9999)]
 
 # The bridged daemon bind address and port to communicate with the site.
 BRIDGED_DJANGO_ADDRESS = [('localhost', 9998)]
