1c6cd637605e3811c0e22d0e36704adb1e84ea93
Jason Cameron <git@jasoncameron.dev>
2024-09-22 19:27:51 -0400

    Add Traefik to load balance between judges
    Signed-off-by: Jason Cameron <git@jasoncameron.dev>

--- automation/add_judge.sh
+++ automation/add_judge.sh
@@ -57,7 +57,9 @@ docker service create \
     --replicas 1 \
     --constraint "$CONSTRAINT" \
     --network wlmoj_judge \
-    --network-alias judge \
+    --label "traefik.enable=true" \
+    --label "traefik.http.routers.${JUDGE_NAME}.rule=Host(\`judge\`)" \
+    --label "traefik.http.services.${JUDGE_NAME}.loadbalancer.server.port=9999" \
     --cap-add SYS_PTRACE \
     --mount type=bind,src=/var/share/problems/,dst=/problems/ \
     ghcr.io/mcpt/wlmoj-judge:latest \
